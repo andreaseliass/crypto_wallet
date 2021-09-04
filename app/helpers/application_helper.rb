7ff4cb34605383e8b55ad99845bcb2777b7b9c04
Andrea Sampaio Elias <andreaase@hotmail.com>
2021-09-03 21:59:40 -0300

    evoluçao

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,15 @@ module ApplicationHelper
     "CRYPTO WALLET APP"
   end
 
+  def ambiente_rails
+    if Rails.env.development?
+      "Desenvolvimento"
+    elsif Rails.env.production?
+      "Produção"
+    else
+      "Teste"
+    end
+  end
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'welcome/index'
+  # get '/inicio', to:'welcome#index'
   resources :coins
 
   root to: 'welcome#index'
